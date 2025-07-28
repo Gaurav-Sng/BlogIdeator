bc931af106a0e61cbb5a78778327727c43798b77
Gaurav-Sng <mysteryofpride@gmail.com>
2025-07-28 22:29:21 +0530

    updated admin.js

--- server/admin.js
+++ server/admin.js
@@ -3,7 +3,7 @@ const admin=require('firebase-admin');
 const path = require('path');
 
 // Load service account key
-const serviceAccount = require(path.resolve("blogideator-firebase-adminsdk-fbsvc-29d1dd1e23.json"));
+const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CONFIG);
 // Prevent duplicate initialization
 if (!admin.apps.length) {
   admin.initializeApp({
